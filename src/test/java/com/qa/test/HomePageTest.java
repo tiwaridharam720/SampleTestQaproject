592c065eefe57174a94d379bb056908d3819bae9
DharamMax5 <dharmendrartiwari@gmail.com>
2025-12-20 14:49:10 +0530

    Add the code for log4j in homePage test

--- src/test/java/com/qa/test/HomePageTest.java
+++ src/test/java/com/qa/test/HomePageTest.java
@@ -5,23 +5,22 @@ import java.io.IOException;
 import org.apache.poi.EncryptedDocumentException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qa.Pages.HomePage;
 import com.qa.testbase.TestBase;
 import com.qa.util.Util;
-
+import org.apache.log4j.Logger;
 public class HomePageTest extends TestBase {
 
 	HomePage homepage;
+	
 	String SheetName="HomePage";
 
-
-
+    Logger log = Logger.getLogger(HomePageTest.class);   
+	
 	public HomePageTest() {
 
 		super();
@@ -30,6 +29,8 @@ public class HomePageTest extends TestBase {
 	@BeforeMethod
 	public void Setup() {
 
+		
+		log.info("****************************Starting the Test Case execustion***********");
 		Initilization();
 
 		homepage = new HomePage();
@@ -38,12 +39,18 @@ public class HomePageTest extends TestBase {
 	@Test(priority =1, enabled = true)
 
 	public void ValidateThePageTitle() {
-
+		log.info("****************************** starting test case *****************************************");
+		log.info("****************************** Your Store *****************************************");
+	
+		
 		String ActualPageTitle = HomePage.ValidatePagetiele();
 
 		System.out.println("Prinnt PageTitle"+ActualPageTitle);
 
 		Assert.assertEquals(ActualPageTitle, "Your Store");
+		log.info("****************************** ending test case *****************************************");
+		log.info("****************************** Your Store *****************************************");
+
 
 
 	}
@@ -72,7 +79,7 @@ public class HomePageTest extends TestBase {
 
 
 
-	@Test(priority =2 , dataProvider="HomePageTestData")
+	@Test(priority =2 , dataProvider="HomePageTestData", enabled =true)
 	 public void doSearchTest(String ProductName) {
 
 		String ActaulHeaderAfterSearch = homepage.doSearch(ProductName);
@@ -88,6 +95,10 @@ public class HomePageTest extends TestBase {
 
 		//Assert.assertEquals(ActaulHeaderAfterSearch, "Search - " +ProductName);
     	Assert.assertEquals(ActaulHeaderAfterSearch, "Search - " + ProductName);
+    	log.info("******************Info Search Product********************");
+    	log.debug("*********************Search Product********************");
+    	log.fatal("*********************Fatal Search Product**************");
+    	log.warn("**********************Waring Search Product*************");
 
 	}
 
@@ -102,6 +113,9 @@ public class HomePageTest extends TestBase {
 				Thread.sleep(2000);
 				driver.quit();
 				Thread.sleep(2000);
+				
+				log.info("****************************** Browser is closed *****************************************");
+
 			} catch (InterruptedException e) {
 				System.out.println("Print Error"+e.getMessage());			
 		}	
